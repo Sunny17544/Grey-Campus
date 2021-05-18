172c8c46f6a07f97ac3a6e7d5333f376a7d17bb0
Sunny17544 <sandeep@greycampus.com>
2021-05-18 15:33:27 +0530

    Ruby-Program: "propogation.rb"

---
+++ [Daily Report] 18th May 2021/Practised Programs/propogation.rb
@@ -0,0 +1,18 @@
+# Write a ruby program to demonstrate propogation.
+
+
+def zen
+    10.times do
+      answer = 42 / 0
+    end
+end
+  
+begin
+    puts "Calling zen."
+    zen
+  rescue ZeroDivisionError => error
+    puts "Rescued from the zen method."
+    puts error.backtrace
+end
+  
+puts "End of main."
